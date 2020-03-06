c656c074d59ef09fb79da2997d29fefdd745a327
marcosrdac <marcosrdac@gmail.com>
2020-03-06 08:07:53 -0300

    almost done...

--- time.py
+++ time.py
@@ -1,8 +1,9 @@
 import re
 import numpy as np
+import matplotlib as mpl
 import matplotlib.pyplot as plt
-#cmap = 'Blues'
-#
+cmap = 'Greens'
+
 mon  = ['monday']   + [ 'segunda' + feira for feira in ['','-feira'] ]
 mon += [ day[:3] for day in mon[:2]]
 tue  = ['tuesday']  + [ 'terça' + feira for feira in ['','-feira'] ]
@@ -33,12 +34,13 @@ def format_interval(interval):
         h1, m1, h2, m2 = interval[0]
         h1 = int(h1) if h1!='' else 0
         m1 = int(m1) if m1!='' else 0
-        h2 = int(h2) if h2!='' else 0
-        m2 = int(m2) if m2!='' else 0
+        h2 = int(h2) if h2!='' else 23
+        m2 = int(m2) if m2!='' else 59 # actually 60
         #interval=f'{h1:.0f}:{m1:.0f} até {h2:.0f}:{m2:.0f}'
         t1 = 60*h1+m1
         t2 = 60*h2+m2-1
     return([t1,t2])
+
 
 def conv_text(times):
     daytimes = re.findall(r'\(([^\)]+)\)|(\S+)', times)
@@ -61,23 +63,82 @@ def conv_text(times):
     for dt, intervals in enumerate(formated_daytimes):
         for i, interval in enumerate(intervals):
             formated_daytimes[dt][i] = format_interval(interval)
-    print(formated_daytimes)
     return(formated_daytimes)
 
 
 #times = "1h... 2-3 13:30-15h (7:50-8 16-17h2 ) seg (-2 ...3) (seg + thu)"
 times = "1h... '' 13:30-15h (7:50-8 16-17h2 ) seg (-2 ...3)"
 
-days = conv_text(times)
+#days = conv_text(times)
 
 grid = np.zeros((24*60,7))
 
+# bota manhã e tarde, bro
+horarios = {
+	"Nilo": "'' 15... '' 13...",
+	"Rada": "13-17 ...10h40 '' tue -12",
+	"Maria": "13-17 seg seg seg seg seg seg",
+	"André": "'' '' '' '' 13-17",
+	"Lorena": "13-14:50 -10:40 seg",
+	"Laian": "'' '' '' 16-16:40",
+	"Anderson": "'' '' '' 5-17",
+	"Vinicius": "13- '' seg (-14 16-)",
+	"Mari": "10:40- 7-14h seg ter -16h40",
+	"Leticia": "12-16:40 '' seg 14:50- seg",
+	"Gutembergue": "14:50-17:00 '' seg '' seg",
+	"Alber": "13-16:40 '' seg 14:50- seg",
+	"Isaac": "10:40- seg seg seg seg",
+	"Rômulo": "13-16h40 '' seg 14:50- seg",
+	"Annie": "16:40- '' '' 10h40-",
+	"Maria M": "'' 15h-16:30 '' 10:40-16:30",
+	"Vasco": "-14:50 '' seg 10h40- seg",
+	"Milton": "9:45-12:30 '' -9:45 10h40-12:30",
+}
 
 
-for d, day in enumerate(days):
-    for times in day:
-        grid[times[0]:times[1], d] += 1
+
+for horario in horarios.values():
+    hdays = conv_text(horario)
+    for d, day in enumerate(hdays):
+        for times in day:
+            grid[times[0]:times[1], d] += 1
 
 
-plt.imshow(grid, aspect='auto')
+data=grid
+col_labels = []
+row_labels = []
+fig, ax = plt.subplots(figsize=(4,6))
+# plot grid heatmap
+im = ax.imshow(data, aspect='auto', interpolation='nearest', cmap=cmap)
+# create colorbar
+cmap = plt.get_cmap(cmap)
+bounds=np.arange(grid.min(),grid.max()+2, dtype=int)
+vals=bounds[:-1]
+norm = mpl.colors.BoundaryNorm(bounds, cmap.N)
+cbar = ax.figure.colorbar(im,
+                        cmap=cmap,
+                        norm=norm,
+                        boundaries=bounds,
+                        orientation='horizontal',
+                        values=vals,
+                        ticks=bounds)
+cbar.set_ticks(vals + .5)
+cbar.set_ticklabels(vals)
+cbar.ax.set_xlabel('Number of people able for a meeting', rotation=0)
+cbar.outline.set_linewidth(0)
+# days axis
+ax.set_xticks(np.arange(data.shape[1]))
+ax.set_xticklabels(['mon' ,'tue','wed','thu','fri','sat','sun'])
+# Let the horizontal axes labeling appear on top.
+ax.tick_params(top=True, bottom=False,
+                labeltop=True, labelbottom=False)
+# Turn spines off and create white grid.
+for edge, spine in ax.spines.items():
+    spine.set_visible(False)
+# We want to show all ticks...
+ax.set_yticks(np.arange(data.shape[0], 60))
+ax.set_yticklabels(np.arange(0,24))
+# ... and label them with the respective list entries.
+#ax.set_xticklabels([day for day, vals in days.items()[0]])
+
 plt.show()
