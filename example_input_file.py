523f333ec0a9c87189d41fd54a2d53e3eb402d62
marcosrdac <marcosrdac@gmail.com>
2020-03-15 10:36:23 -0300

    changing some pics

--- example_input_file.py
+++ example_input_file.py
@@ -7,9 +7,10 @@ input_file = 'example_input_file.txt'
 people_free_schedule = file2dict(input_file)
 
 plot_free_schedule(people_free_schedule,
-                   first_day='sunday', last_day='saturday',
                    header="Ardur" + '\n' + \
                           "When to play RPG",
+                   cbar_label="Number of people able to play RPG",
+                   first_day='sunday', last_day='saturday',
                    start_time='7:30', end_time='24',
                    cmap='PuRd',
                    format24=True,
